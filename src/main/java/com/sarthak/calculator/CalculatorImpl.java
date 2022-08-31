bbcbdc84f5043576cc37bd2e8b458c4024a5a52b
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-08-31 19:30:52 +0530

    initialised the scanner

--- src/main/java/com/sarthak/calculator/CalculatorImpl.java
+++ src/main/java/com/sarthak/calculator/CalculatorImpl.java
@@ -5,9 +5,16 @@
  */
 package com.sarthak.calculator;
 
+import java.util.Scanner;
+
 public class CalculatorImpl {
     public static void main(String[] args) {
         // Create and object for the calculator class
         Calculator calculator = new Calculator();
+        // Initialise the scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Close the scanner
+        scanner.close();
     }
 }
