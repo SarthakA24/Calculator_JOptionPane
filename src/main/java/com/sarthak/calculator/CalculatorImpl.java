e85ee950515dfbbb15e4dcf143bb0f9535da86a2
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-08-31 20:30:29 +0530

    edited the input field to directly call the setter method

--- src/main/java/com/sarthak/calculator/CalculatorImpl.java
+++ src/main/java/com/sarthak/calculator/CalculatorImpl.java
@@ -14,27 +14,34 @@ public class CalculatorImpl {
         Calculator calculator = new Calculator();
         // Initialise the scanner
         Scanner scanner = new Scanner(System.in);
-        // Declare the object for the ImageIcon class
-        ImageIcon icon = new ImageIcon("src/main/resources/images/Calculator-icon.png");
         // Prompt the user to enter the number using JOptionPane
         double input = Double.parseDouble((String) JOptionPane.showInputDialog(
                 null,
                 "Enter the number",
                 "Calculator",
                 JOptionPane.PLAIN_MESSAGE,
-                icon,
+                Calculator.getIcon(),
                 null,
                 null
         ));
         // Prompt the user to enter the operation using JOptionPane
-        String operation = (String) JOptionPane.showInputDialog(
+//        String operation = (String) JOptionPane.showInputDialog(
+//                null,
+//                "Enter the operation",
+//                "Calculator",
+//                JOptionPane.PLAIN_MESSAGE,
+//                Calculator.getIcon(),
+//                new String[]{"Add", "Subtract", "Multiply", "Divide"},
+//                null
+//        );
+        calculator.setOperation((String) JOptionPane.showInputDialog(
                 null,
                 "Enter the operation",
                 "Calculator",
                 JOptionPane.PLAIN_MESSAGE,
-                icon,
+                Calculator.getIcon(),
                 new String[]{"Add", "Subtract", "Multiply", "Divide"},
-                null
+                null)
         );
         // Close the scanner
         scanner.close();
