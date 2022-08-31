52b88a2a78f95606c3283f452d23ef85fc46aa20
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-08-31 19:30:17 +0530

    created an object for the calculator class

--- src/main/java/com/sarthak/calculator/CalculatorImpl.java
+++ src/main/java/com/sarthak/calculator/CalculatorImpl.java
@@ -7,6 +7,7 @@ package com.sarthak.calculator;
 
 public class CalculatorImpl {
     public static void main(String[] args) {
-
+        // Create and object for the calculator class
+        Calculator calculator = new Calculator();
     }
 }
