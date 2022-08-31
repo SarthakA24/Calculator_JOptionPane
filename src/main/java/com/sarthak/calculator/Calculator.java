bff7cae7d17d6f795ba5d119eaca5a4af298a993
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-08-31 20:13:04 +0530

    added attributes for Calculator type

--- src/main/java/com/sarthak/calculator/Calculator.java
+++ src/main/java/com/sarthak/calculator/Calculator.java
@@ -6,6 +6,11 @@
 package com.sarthak.calculator;
 
 public class Calculator {
+    private double sum;
+    private double subtract;
+    private double multiply;
+    private double divide;
+
     public double add(double... numbers) {
         double sum = 0;
         for (double number : numbers) {
