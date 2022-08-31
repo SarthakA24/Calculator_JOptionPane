34b087adf365f71524c42f62552972010ae08c25
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-08-31 20:31:12 +0530

    removed the scanner class

--- src/main/java/com/sarthak/calculator/CalculatorImpl.java
+++ src/main/java/com/sarthak/calculator/CalculatorImpl.java
@@ -6,14 +6,11 @@
 package com.sarthak.calculator;
 
 import javax.swing.*;
-import java.util.Scanner;
 
 public class CalculatorImpl {
     public static void main(String[] args) {
         // Create and object for the calculator class
         Calculator calculator = new Calculator();
-        // Initialise the scanner
-        Scanner scanner = new Scanner(System.in);
         // Prompt the user to enter the number using JOptionPane
         double input = Double.parseDouble((String) JOptionPane.showInputDialog(
                 null,
@@ -43,7 +40,5 @@ public class CalculatorImpl {
                 new String[]{"Add", "Subtract", "Multiply", "Divide"},
                 null)
         );
-        // Close the scanner
-        scanner.close();
     }
 }
