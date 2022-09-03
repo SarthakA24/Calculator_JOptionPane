f66e7140497b62201e0fdb4b15dacce9f2d661cf
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-03 22:45:56 +0530

    reformatted the code

--- src/main/java/com/sarthak/calculator/CalculatorImpl.java
+++ src/main/java/com/sarthak/calculator/CalculatorImpl.java
@@ -15,28 +15,11 @@ public class CalculatorImpl {
         // Prompt the user to enter the number using JOptionPane
         JTextField firstNumber = new JTextField();
         JTextField secondNumber = new JTextField();
-        Object[] numbersInput = {
-                "First Number : ", firstNumber,
-                "Second Number : ", secondNumber
-        };
+        Object[] numbersInput = {"First Number : ", firstNumber, "Second Number : ", secondNumber};
         String[] operations = {"Add", "Subtract", "Multiply", "Divide"};
         do {
-            String operation = String.valueOf(JOptionPane.showInputDialog(
-                    null,
-                    "Select the operation you need to perform",
-                    "Calculator",
-                    JOptionPane.ERROR_MESSAGE,
-                    Calculator.getIcon(),
-                    operations,
-                    "Add"));
-            int calculatorOption = JOptionPane.showConfirmDialog(
-                    null,
-                    numbersInput,
-                    "Calculator",
-                    JOptionPane.YES_NO_OPTION,
-                    JOptionPane.PLAIN_MESSAGE,
-                    Calculator.getIcon()
-            );
+            String operation = String.valueOf(JOptionPane.showInputDialog(null, "Select the operation you need to perform", "Calculator", JOptionPane.ERROR_MESSAGE, Calculator.getIcon(), operations, "Add"));
+            int calculatorOption = JOptionPane.showConfirmDialog(null, numbersInput, "Calculator", JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, Calculator.getIcon());
             if (calculatorOption == JOptionPane.OK_OPTION) {
                 boolean isOperationSetSuccess = calculator.setOperation(operation);
                 if (isOperationSetSuccess) {
